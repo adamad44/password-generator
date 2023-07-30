d5f6fde6afcd20bee2059ab025a14de2944e6dbb
adamad44 <140947818+adamad44@users.noreply.github.com>
2023-07-30 16:28:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Password Generator
+
+## Introduction
+
+This is a simple password generator application created using Python and Tkinter. It allows users to generate random passwords of varying lengths and copy them to the clipboard for easy use.
+
+## Features
+
+- Generate random passwords using a combination of letters, numbers, and special characters.
+- Choose the length of the password using a slider.
+- Copy the generated password to the clipboard with a single click.
+
+## Requirements
+
+- Python 3.x
+
+## How to Use
+
+1. Make sure you have Python 3.x installed on your system.
+2. Clone or download the repository to your local machine.
+
+## Running the Password Generator
+
+To run the password generator, execute the following command in your terminal or command prompt:
+python main.py
+
+The password generator window will appear with the following elements:
+
+- "Choose Length": Use the slider to select the desired length of the password.
+- "Generate": Click this button to generate a random password based on the chosen length.
+- "Copy": After generating a password, click this button to copy it to the clipboard.
+
+The generated password will be displayed below the "Copy" button, and you can paste it into any text field as needed.
+
+## Note
+
+- The password generator uses a combination of lowercase letters, uppercase letters, numbers, and special characters to create strong passwords.
+- The generated passwords are random and should be used for temporary purposes or where password strength is not a critical concern.
+- The password is copied to the clipboard automatically when you click the "Copy" button. Make sure to paste it securely and avoid sharing it in an insecure environment.
+
+Enjoy using the Password Generator! If you encounter any issues or have suggestions for improvement, feel free to submit an issue or create a pull request.
+
---
+++ main.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+from tkinter import *
+import random
+import pyperclip
+
+# Create the main Tkinter window
+root = tk.Tk()
+root.title('Password Generator')
+root.geometry('600x500')
+root.config(bg='#252525')
+
+# Define the characters that can be used to generate the password
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', ':', ';', ',', '.', '<', '>', '/', '?']
+
+# Variable to hold the generated password
+password_var = StringVar()
+
+# Function to generate a random password
+def generate():
+    # Shuffle the characters to get a random order
+    random.shuffle(chars)
+    # Get the chosen length of the password from the slider
+    length = int(length_slider.get())
+    # Take a sample of the shuffled characters based on the chosen length
+    generated_password = ''.join(random.sample(chars, length))
+    # Set the generated password in the password_var variable
+    password_var.set(generated_password)
+
+# Function to copy the generated password to the clipboard
+def copy_to_clipboard():
+    result_text = password_var.get()
+    pyperclip.copy(result_text)
+
+# Create and configure the GUI elements
+title = Label(root, text='Password Generator', font='Verdana 25 bold', bg='#252525', fg='white')
+title.pack(pady=20)
+
+length_label = Label(root, text='Choose Length:', font='Verdana 14', bg='#252525', fg='white')
+length_label.pack()
+
+length_slider = Scale(root, from_=1, to=50, orient=HORIZONTAL, length=200, bg='#252525', fg='white', highlightbackground='#252525', sliderlength=20)
+length_slider.pack()
+
+gen_button = Button(root, text='Generate', font='Verdana 14', command=generate, bg='#4caf50', fg='white', activebackground='#45a049', activeforeground='white')
+gen_button.pack(pady=10)
+
+copy_button = Button(root, text='Copy', font='Verdana 14', command=copy_to_clipboard, bg='#007BFF', fg='white', activebackground='#0056b3', activeforeground='white')
+copy_button.pack(pady=10)
+
+result = Label(root, textvariable=password_var, font='Verdana 16', bg='#252525', fg='white')
+result.pack(pady=20)
+
+# Start the Tkinter event loop
+root.mainloop()
